2c63e7d1c814d00f9ae0ec44434096f84d7f8ffa
CanhCo <110582659+CanhCo@users.noreply.github.com>
2022-08-25 16:27:46 +0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+locals {
+    project = "terraform-modules"
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+    access_key = "AKIAVA6GRNJYZ2GQYAEF"
+    secret_key = "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"
+}
+
+module "networking" {
+    source = "./modules/networking"
+
+    project = local.project
+    vpc_cidr         = "10.0.0.0/16"
+    private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    public_subnets   = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+    database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+}
+module "database" {
+    source = "./modules/database"
+
+    project = local.project
+    vpc     = module.networking.vpc
+    sg      = module.networking.sg
+}
+
+module "autoscaling" {
+    source = "./modules/autoscaling"
+
+    project   = local.project
+    vpc       = module.networking.vpc
+    sg        = module.networking.sg
+    db_config = module.database.db_config
+    
+}
